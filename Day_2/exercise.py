2e14cb43d204cbb4e717e46cd1151e4e576fb119
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2024-03-26 23:07:47 +0100

    day two activites

--- Day_2/exercise.py
+++ Day_2/exercise.py
@@ -1,31 +1,31 @@
-# two_digit_number = input()
+two_digit_number = input()
 
 
-# a = str(two_digit_number)
-# # b = float([*a])[1] + float([*a])[2]
-# int1 = int([*a][0])
-# int2 = int([*a][1])
+a = str(two_digit_number)
+# b = float([*a])[1] + float([*a])[2]
+int1 = int([*a][0])
+int2 = int([*a][1])
 
-# total = int1 + int2
+total = int1 + int2
 
-# print(total)
+print(total)
 
-# print( 2 ** 3)
+print( 2 ** 3)
 
-# # 1st input: enter height in meters e.g: 1.65
-# height = input()
-# # 2nd input: enter weight in kilograms e.g: 72
-# weight = input()
-# # 🚨 Don't change the code above 👆
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
 
-# # Write your code below this line 👇
+# Write your code below this line 👇
 
-# height_sqr = float(height) ** 2
-# float_weight = float(weight)
+height_sqr = float(height) ** 2
+float_weight = float(weight)
 
-# BMI = float_weight / height_sqr
+BMI = float_weight / height_sqr
 
-# print(round(BMI))
+print(round(BMI))
 
 age = input()
 
